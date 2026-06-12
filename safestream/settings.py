70a9c1578dbcb0183817c694c6db72c2d19524b3
Munal Singh <munalsingh42@gmail.com>
2026-06-12 11:22:27 +0530

    Render deployment settings

--- safestream/settings.py
+++ safestream/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p4=7oj+kb==2&fvtdrhg4((3ad3y2hbearo(l0$v!e4k11iro$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,8 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
